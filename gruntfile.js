c6c265157d8467f5e0a66af3cfad4f20263eea50
Nick Spiel <nick.spiel@digital360.com.au>
2015-01-23 19:07:35 +1100

    Updated grunt js tasks and added cssmin

--- gruntfile.js
+++ gruntfile.js
@@ -1,17 +1,93 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-		// Configure nodemon
+		
+		// Read package file
+		pkg: grunt.file.readJSON('package.json'),
+
+		// -------------------------------------------------- Tasks
+
+		// ------------------------- CSS tasks
+
+		// Convert sass files to css
+		sass: {
+			build: {
+				options: {
+					loadPath: require('node-bourbon').includePaths,
+					style: 'expanded'
+				},
+				files: {
+					'<%= pkg.stylesDirectory.public %>/global.css': '<%= pkg.stylesDirectory.app %>/global.scss'
+				}
+			}
+		},
+
+		// Minify css files
+		cssmin: {
+			build: {
+				files: {
+					'<%= pkg.stylesDirectory.public %>/global.min.css': '<%= pkg.stylesDirectory.public %>/global.css'
+				}
+			}
+		},
+
+		// ------------------------- Javascript tasks
+
+		// Check all js files for errors
+		jshint: {
+			all: ['<%= pkg.scriptsDirectory.app %>/*.js']
+		},
+
+		// Minify all js files into app.min.js
+		uglify: {
+			build: {
+				files: {
+					'<%= pkg.scriptsDirectory.public %>/app.min.js': ['<%= pkg.scriptsDirectory.app %>/*.js']
+				}
+			}
+		},
+
+		// -------------------------------------------------- Operations 
+
+		// Watch node server for changes
 		nodemon: {
 			dev: {
-				script: 'app/server.js'
+				script: 'server.js'
 			}
-		}
+		},
+		
+		// Watch files for changes
+		watch: {
+			css: {
+				files: [
+					'<%= pkg.stylesDirectory.app %>/*.scss'
+				],
+				tasks: ['sass', 'cssmin']
+			},
+			js: {
+				files: ['public/src/js/**/*.js'],
+				tasks: ['jshint', 'uglify']
+			}
+		},
+		
+		// Run watch and nodemon at the same time
+		concurrent: {
+			options: {
+				logConcurrentOutput: true
+			},
+			tasks: ['nodemon', 'watch']
+		},
 	});
 
-	// Load nodemon
+	// Load modules
+	grunt.loadNpmTasks('grunt-concurrent');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-nodemon');
 
 	// Register the nodemon task when we run grunt
-	grunt.registerTask('default', ['nodemon']);  
+	grunt.registerTask('default', ['sass', 'cssmin', 'jshint', 'uglify', 'concurrent']);  
 };
