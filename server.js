115e16069e283fe7cd5599c87a18dacae8124477
Rob Petreski <robert.petreski@digital360.com.au>
2015-01-24 23:28:21 +1100

    Added conditional environments, error reporting, and starting passport.js

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 var express = require('express'),
-	app     = express();
+	http = require('http'),
+	session = require('express-session'),
+	passport = require('passport'),
+	passportLocal = require('passport-local'),
+	passportGoogle = require('passport-google');
+
+// Define node app
+var app = express();
 
 // Setting the root directory
 app.use(express.static(__dirname + '/public'));
 
 // Setting the start file
+// TODO: Put routes in routes file
 app.get('/', function(req, res) {
 	res.sendFile('index.html', { root: __dirname });
 });
 
-// Listening port
-app.listen(8080);
+// Set up dev environment handling
+// TODO: Create environments file
+var env = process.env.NODE_ENV;
+
+if (env == null) {
+	console.log('No Environments found! App terminated.');
+	process.exit(1);
+} else {
+	if ('development' == env) {
+		// Error handling
+		// http://expressjs.com/guide/error-handling.html
+		app.use(function(err, req, res, next){
+			console.error(err.stack);
+			res.status(500).send('Server Error');
+		});
+
+		// Also see https://github.com/trentm/node-bunyan
+	}
+}
