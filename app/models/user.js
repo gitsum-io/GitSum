18cc14a0af12f3bdcf179b3e924e7f8d6c9e4e8d
Nick Spiel <nick.spiel@digital360.com.au>
2015-02-09 07:49:46 +1100

    UPdated database and user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,32 @@
+// --------------------------------------- Modules
+
 var mongoose = require('mongoose');
 
-// TODO: rename this to user (was using person as I had test data in my db)
+// --------------------------------------- Model
 
-module.exports = mongoose.model('Person', {
-    name : {
-    	type : String, 
-    	default : ''
-    }
+var Schema   = mongoose.Schema;
+
+// User schema
+var UserSchema = new Schema({
+	email: String,
+	name:  String
 });
+
+// User model
+var User = mongoose.model('User', UserSchema);
+
+// Expose user model
+module.exports = User;
+
+// TODO: remove the below when ready
+
+// // New User
+// var nick = new User({
+// 	email: "email@nicksdassadsadpiel.me",
+// 	name: "nick",
+// });
+
+// // Save User to the DB
+// nick.save(function(err, nick) {
+//   if (err) throw err;
+// });
