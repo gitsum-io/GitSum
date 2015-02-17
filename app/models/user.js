56e4a12b4ea434c5c9dab8691df289c3d54986f8
Rob Petreski <robert.petreski@digital360.com.au>
2015-02-17 13:15:13 +1100

    Added test user for testing purposes

--- app/models/user.js
+++ app/models/user.js
@@ -46,3 +46,68 @@ var User = mongoose.model('User', UserSchema);
 
 // Expose user model
 module.exports = User;
+
+var testUser = new User;
+testUser.name = "John Doe";
+testUser.email = "me@example.com";
+testUser.repositories = [
+			{
+				"name": "Alcoil",
+				"branches": [
+					{
+						"name": "master",
+						"head": true,
+						"commits": [
+							{
+								"hash": "ayugwerljkdahjksd",
+								"date": "1/2/2015",
+								"message": "Latest commit message!",
+								"contributer": "Nick Spiel",
+								"avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/nickspiel/128.jpg"
+							},
+							{
+								"hash": "hdfshjdfshjkdfskhj",
+								"date": "29/1/2015",
+								"message": "Why am I commiting code?!",
+								"contributer": "Ash Brock",
+								"avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/gerrenlamson/128.jpg"
+							}
+						]
+					},
+					{
+						"name": "dev",
+						"head": true,
+						"commits": [
+							{
+								"hash": "ayugwerljkdahjksd",
+								"date": "1/2/2016",
+								"message": "First commit message!",
+								"contributer": "Robert Petreski",
+								"avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/soffes/128.jpg"
+							},
+							{
+								"hash": "ayugwerljkdahjksd",
+								"date": "1/2/2016",
+								"message": "First commit message!",
+								"contributer": "Robert Petreski",
+								"avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/soffes/128.jpg"
+							}
+						],
+					}
+				]
+			},
+			{
+				"name": "Myskin"
+			}
+		];
+
+// Check if temp user already exists
+// in DB otherwise save it
+User.find({ name: 'John Doe' }).find(function(err, user) {
+	if (!user.length) {
+		testUser.save();
+		console.log('Temp User saved!');
+	} else {
+		console.log('Temp User found!');
+	}
+})
