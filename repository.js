b2cc55298d1bc42f78d8c0212b91e55e13f72d97
Robert Petreski <robert.petreski@digital360.com.au>
2016-09-26 13:05:55 +1000

    Added repository controller

---
+++ repository.js
@@ -0,0 +1,87 @@
+function RepositoryController() {
+    var repo = this;
+
+    // Set up an empty array to store some test data
+    repo.store = [];
+
+    // Find a repo by ID
+    var findRepositoryById = function(req) {
+        var found = repo.store.filter(function(p) {
+            return p.id === parseInt(req.params.id);
+        });
+
+        if (found && found.length > 0) {
+            return found[0];
+        }
+
+        return null;
+    };
+
+    // Get method
+    repo.get = function(req, res, next) {
+        res.send(200, repo.store);
+        return next();
+    };
+
+    // Method for getting the repo by ID
+    repo.getById = function(req, res, next) {
+        var found = findRepositoryById(req);
+
+        if (found) {
+            res.send(200, found);
+        } else {
+            res.send(404, 'Repository not found!')
+        }
+
+        return next();
+    };
+
+    // Add method
+    // Added test validation
+    repo.post = function(req, res, next) {
+        if (!req.body.hasOwnProperty('id') || !req.body.hasOwnProperty('name')) {
+            res.send(500);
+            return next();
+        } else {
+            repo.store.push({
+                id: parseInt(req.body.id),
+                name: req.body.name
+            });
+            res.send(201);
+        }
+
+        return next();
+    };
+
+    // Method for updating a repository
+    repo.put = function(req, res, next) {
+        if (!req.body.hasOwnProperty('name')) {
+            res.send(500);
+            return next();
+        }
+
+        var found = findRepositoryById(req);
+
+        if (found) {
+            found.name = req.body.name;
+            res.send(200, found);
+        } else {
+            res.send(404, 'Repository not found!');
+        }
+
+        return next();
+    };
+
+    // Delete method for repository
+    repo.del = function(req, res, next) {
+        repo.store = repo.store.filter(function(p){
+            return p.id !== parseInt(req.params.id);
+        });
+
+        res.send(200);
+
+        return next();
+    };
+};
+
+module.exports = new RepositoryController();
