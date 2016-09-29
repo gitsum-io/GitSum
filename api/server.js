d10f68b700fa4b3b56b5b0c4690da32b8d8bb250
Robert Petreski <robert.petreski@digital360.com.au>
2016-09-29 13:16:13 +1000

    Updated mongoose to its own location

--- api/server.js
+++ api/server.js
@@ -1,12 +1,8 @@
 var restify = require('restify'),
-    mongoose = require('mongoose/'),
-    environment = require('./environment.json'),
     repository = require('./repository'),
     port = process.env.PORT || 3000;
 
-// Connect Database
-var db = mongoose.connect(environment.credentials.mongodb.uri),
-    Schema = mongoose.Schema;
+var repositoryModel = require('./models/repository');
 
 var server = restify.createServer({
   name: 'GitSum API V1'
