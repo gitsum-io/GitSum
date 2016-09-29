3393e893bf90d3a7797d662a7970e8a3a728e13b
Robert Petreski <robert.petreski@digital360.com.au>
2016-09-29 13:29:52 +1000

    Added extra required fields

--- api/models/repository.js
+++ api/models/repository.js
@@ -1,11 +1,11 @@
-// TODO: Add in uri_type to denote SSH or HTTPS
-
 function RepositoryModel() {
     var db = require('../database/db.js');
 
     var repositorySchema = new db.schema({
         name: String,
-        uri: String
+        uri: String,
+        uri_type: String,
+        type: String
     });
 
     db.mongoose.model('Repository', repositorySchema);
