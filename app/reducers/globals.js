bd17a8de22aafd3cfff27ef9b17956c13ac480ac
Nick Spiel <nick.spiel@digital360.com.au>
2016-10-13 08:19:30 +1100

    Global is now globals

--- app/reducers/globals.js
+++ app/reducers/globals.js
@@ -4,6 +4,14 @@ export default function global(state = {}, action) {
     return Object.assign({}, state, {
       name: action.names
     })
+  case 'ACTIVATE_ADD_FORM':
+    return Object.assign({}, state, {
+      addFormActive: true
+    })
+  case 'DEACTIVATE_ADD_FORM':
+    return Object.assign({}, state, {
+      addFormActive: false
+    })
   default:
     return state
   }
