813c5df123dd6214361c6aa6c35ed8a7207f3c77
Robert Petreski <robert.petreski@digital360.com.au>
2016-10-13 13:11:43 +1000

    Update to include user controller and routes

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 var restify = require('restify'),
     repository = require('./controllers/repository'),
+    user = require('./controllers/user'),
     port = process.env.PORT || 3000;
 
 var server = restify.createServer({
@@ -15,20 +16,34 @@ server.use(restify.queryParser());
 server.use(restify.fullResponse());
 server.use(restify.bodyParser());
 
-// Get a list of all repos
-server.get('/api/v1/repository', repository.get);
+/***************************************
+Routes
+TODO: Move into own location
+****************************************/
 
+// Repositories
+// Get a list of all repos
+server.get('/api/v1/repositories', repository.get);
 // Update existing repo
 server.put('/api/v1/repository/:id', repository.put);
-
 // Add a new repo
 server.post('/api/v1/repository', repository.post);
-
 // Get a specific repo
 server.get('/api/v1/repository/:id', repository.getById);
-
-// Get a specific repo
+// Delete a specific repo
 server.del('/api/v1/repository/:id', repository.del);
+
+// Users
+// Get a list of all users
+server.get('/api/v1/users', user.get);
+// Update existing user
+server.put('/api/v1/user/:id', user.put);
+// Add a new user
+server.post('/api/v1/user', user.post);
+// Get a specific user
+server.get('/api/v1/user/:id', user.getById);
+// Delete a specific user
+server.del('/api/v1/user/:id', user.del);
 
 // Set up the server for listening
 server.listen(port, function() {
