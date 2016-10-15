4bade4b33c2f8c4f45cb76f94e1e61d83d066b6c
Robert Petreski <robert.petreski@digital360.com.au>
2016-10-15 15:03:16 +1100

    Updated with some beginning work

--- api/controllers/response.js
+++ api/controllers/response.js
@@ -1,2 +1,18 @@
 // Response object
 // TODO
+
+function Response() {
+
+    var response = this;
+
+    response.message = '';
+
+    if (response.message != '') {
+        
+    } else {
+        res.send(500, '{"message": "Internal server error"}');
+        console.log("Response message not provided!");
+    }
+}
+
+module.exports = new RepositoryModel();
