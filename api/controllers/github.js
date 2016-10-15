f5472aa84e6acccee61c85d3d6df8b833cb53d0f
Robert Petreski <robert.petreski@digital360.com.au>
2016-10-15 15:34:35 +1100

    Updated with better comments and method to retrieve branches

--- api/controllers/github.js
+++ api/controllers/github.js
@@ -34,12 +34,16 @@ function GitHubController() {
 
     // Grab access token from GitHub via code provided
     github.authorize = function(req, res, next) {
-        var code = req.params.code;
+        // URL params
         var userId = req.params.userid;
+
+        // Json body params
+        var code = req.params.code;
         var clientId = req.params.client_id;
         var clientSecret = secrets.github.client_secret;
         var scopes = req.params.scopes;
         var state = req.params.state;
+
         var accessToken = '';
 
         // Get current access token from user
@@ -103,7 +107,10 @@ function GitHubController() {
 
     // Method for updating a repository
     github.getRepository = function(req, res, next) {
+        // URL params
         var repoId = req.params.repoid;
+        var userId = req.params.userid;
+        var branch = req.params.branch;
 
         var query = repositories.data.findOne({_id: repoId });
 
@@ -131,6 +138,8 @@ function GitHubController() {
                 var requestURL = 'https://api.github.com/repos'
                     + '/' + repoOwner + '/'
                     + '/' + repoName + '/'
+                    + 'branches' +
+                    + '/' + branch + '/';
 
                 request({
                     url: requestURL,
@@ -140,8 +149,62 @@ function GitHubController() {
                     headers: {
                         'User-Agent': appName
                     }
-                }, function(err, res) {
-                    console.log(res.body);
+                }, function(err, requestedRepo) {
+                    if (requestedRepo.statusCode == 200) {
+                        res.send(200, querystring.parse(requestedRepo.body));
+                    }
+                });
+            });
+        });
+
+        return next();
+    };
+
+    github.getBranches = function(req, res, next) {
+        // URL params
+        var repoId = req.params.repoid;
+        var userId = req.params.userid;
+
+        var query = repositories.data.findOne({_id: repoId });
+
+        query.exec(function(err, repository) {
+            if (err) {
+                res.send(400, JSON.stringify(err));
+            }
+
+            var repoOwner = repository.owner;
+            var repoName = repository.name;
+
+            // Get current access token from user
+            var query = user.data.findOne({_id: userId });
+
+            // Get the access token
+            query.exec(function(err, user) {
+                if (err) {
+                    res.send(400, JSON.stringify(err));
+                }
+
+                // Get the access token from the user's DB
+                var accessToken = user.github_access_token;
+
+                // Build the request URL for github access token
+                var requestURL = 'https://api.github.com/repos'
+                    + '/' + repoOwner + '/'
+                    + '/' + repoName + '/'
+                    + 'branches';
+
+                request({
+                    url: requestURL,
+                    auth: {
+                        'bearer': accessToken
+                    },
+                    headers: {
+                        'User-Agent': appName
+                    }
+                }, function(err, requestedBranches) {
+                    if (requestedBranches.statusCode == 200) {
+                        res.send(200, querystring.parse(requestedBranches.body));
+                    }
                 });
             });
         });
