62419a077903ba496f3a57e690ca9d57f35bb689
Nick Spiel <nick.spiel@digital360.com.au>
2016-11-03 18:58:07 +1100

    Connecting login form to store

--- app/components/login-form/index.js
+++ app/components/login-form/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './styles.css'
 import GitHubIcon from 'assets/images/octocat-inverted.svg'
+import { connect } from 'react-redux'
 
 // TODO Get repo settings from backend for posting to Github
 // TODO Style github button
@@ -14,20 +15,21 @@ const LoginForm = React.createClass({
     }),
     getStateToken: React.PropTypes.func
   },
-  componentWillMount() {
-    this.props.getStateToken() // TODO Should this be a part of the gitsum file and app bootstrapping?
-  },
   render() {
     const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=183eb9754ab178e57b49&scope=repo%20user&state=${this.props.globals.stateToken}`
     return (
       <div className={styles.loginForm}>
         <h1 className={styles.heading}>{this.props.heading}</h1>
         <form className={styles.form} onSubmit={this.handleSubmit}>
-          <a className={styles.githubButton} href={githubAuthUrl}><GitHubIcon className={styles.githubIcon} />Log in with Github</a>
+          <a className={styles.githubButton} href={githubAuthUrl}><GitHubIcon className={styles.githubIcon} />Sign in with Github</a>
         </form>
       </div>
     )
   }
 })
 
-export default LoginForm
+function mapStateToProps(state) {
+  return { globals: state.globals }
+}
+
+export default connect(mapStateToProps)(LoginForm)
