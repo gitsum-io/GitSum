9c810709b48dbcf09b9c7cb872d939d8ee34d07e
Nick Spiel <nick.spiel@digital360.com.au>
2016-11-03 19:01:47 +1100

    Cleaning up AuthModal

--- app/views/login/index.js
+++ app/views/login/index.js
@@ -4,16 +4,13 @@ import AuthModal from 'components/auth-modal'
 import Jungle from 'assets/images/jungle.jpg'
 
 const Login = React.createClass({
-  propTypes: {
-    globals: React.PropTypes.object
-  },
   render() {
     const backgroundStyle = {
       backgroundImage: 'url(' + Jungle + ')',
     }
     return (
       <div className={styles.main} style={backgroundStyle}>
-        <AuthModal {...this.props} />
+        <AuthModal />
       </div>
     )
   }
